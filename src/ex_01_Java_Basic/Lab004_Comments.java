7bbb47b66f0fde3896222cfab5cd6d161355e376
vermav27 <verma.vineet27@gmail.com>
2025-01-06 10:54:14 +0530

    lab003,lab004,lab005

---
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+
+    public static void main(String[] args) {
+
+        //This is a comment, I will be ignored on execution ( Single Line Comment )
+
+    }
+
+}
---
+++ src/ex_01_Java_Basic/Lab005_MultiLineComment.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab005_MultiLineComment {
+
+    public static void main(String[] args) {
+
+        /*
+        Hi, this block is a multi line comment.
+        In between these block, everything will
+        be ignored from execution.
+         */
+
+    }
+
+}
