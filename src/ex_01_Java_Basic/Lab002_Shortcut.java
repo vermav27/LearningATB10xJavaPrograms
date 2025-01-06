78eac6fa64d11eb5ca5d5255ba5e987443880525
vermav27 <verma.vineet27@gmail.com>
2025-01-06 09:38:53 +0530

    lab002_Shortcut

---
+++ src/ex_01_Java_Basic/Lab002_Shortcut.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Shortcut {
+
+    //type "main" and press enter to get the main function
+    public static void main(String[] args) {
+
+        //type "sout" and press enter to get system.out.println statement
+        System.out.println("Hi, Vineet");
+
+    }
+
+}
