ba4d0ce8e38adf5e6429a2dfde8c1f87b20b2186
vermav27 <verma.vineet27@gmail.com>
2025-01-07 09:00:21 +0530

    lab006_modified

--- src/ex_01_Java_Basic/Lab006_ShortcutComments.java
+++ src/ex_01_Java_Basic/Lab006_ShortcutComments.java
@@ -8,10 +8,10 @@ public class Lab006_ShortcutComments {
         System.out.println("This is a shortcut program");
 
         //for multiline comment use shortcut " ctrl + shift + /"
-        System.out.println("My");
+        /*System.out.println("My");
         System.out.println("Name");
         System.out.println("Is");
-        System.out.println("Vineet");
+        System.out.println("Vineet");*/
 
     }
 
