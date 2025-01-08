3f5fca98adb57d94d965e7557305bd60569ec20c
vermav27 <verma.vineet27@gmail.com>
2025-01-08 09:11:24 +0530

    lab000 done & added file lab010

---
+++ src/ex_01_Java_Basic/Lab009_CommonMath_Exception.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab009_CommonMath_Exception {
+
+    public static void main(String[] args) {
+
+        System.out.println(10/0);
+        /*
+        Exception -> Exception is abnormal behaviour in the program
+        which disturbs the normal flow.
+        -----> java.lang.ArithmeticException <-----
+         */
+    }
+
+}
---
+++ src/ex_01_Java_Basic/Lab010_println_print.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Lab010_println_print {
+
+    public static void main(String[] args) {
+
+        //Using println --> It prints in a new line.
+        System.out.println("This is a line");
+
+        //Using print --> It just print and donot take care of the new line.
+        System.out.print("This");
+        System.out.print("is");
+        System.out.print("print");
+        System.out.print("code");
+
+    }
+
+}
