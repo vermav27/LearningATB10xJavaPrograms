3562a88125924ad5dbc96d6d3a2fa0e3bdb5a818
vermav27 <verma.vineet27@gmail.com>
2025-01-21 08:55:45 +0530

    Updated Task from x variable to marks

--- src/ex_02_Java_Advance/Lab017_TSK_TernaryOperator.java
+++ src/ex_02_Java_Advance/Lab017_TSK_TernaryOperator.java
@@ -13,7 +13,7 @@ public class Lab017_TSK_TernaryOperator {
     */
 
         int marks = 60;
-        String result = ( x >= 0 && x <= 59 ) ? "F" : (( x >= 60 && x <= 69 ) ? "D" : (( x >= 70 && x <= 79) ? "C" : ( (x >= 80 && x <= 89) ? "B" : "A" ) )) ;
+        String result = ( marks >= 0 && marks <= 59 ) ? "F" : (( marks >= 60 && marks <= 69 ) ? "D" : (( marks >= 70 && marks <= 79) ? "C" : ( (marks >= 80 && marks <= 89) ? "B" : "A" ) )) ;
         System.out.println("Grade is : " + result);
 
     }
