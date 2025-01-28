7e8cb8bd67aeedc01c0ca7678d4e12d831a7fe40
vermav27 <verma.vineet27@gmail.com>
2025-01-28 08:55:56 +0530

    comments

--- src/ex_02_Java_Advance/Lab026_TSK_ReverseInteger.java
+++ src/ex_02_Java_Advance/Lab026_TSK_ReverseInteger.java
@@ -11,9 +11,9 @@ public class Lab026_TSK_ReverseInteger {
             int value = sc.nextInt();
             int reversedValue = 0;
             while(value != 0){
-                int lastDigit = value%10;
-                reversedValue = reversedValue * 10 + lastDigit;
-                value = value / 10;
+                int lastDigit = value%10; // taking out last digit of number
+                reversedValue = reversedValue * 10 + lastDigit;  //making number reverse
+                value = value / 10; //taking out rest of number to again extracting out last value
             }
             System.out.println("This is the reversed value : " + reversedValue);
         }else{
