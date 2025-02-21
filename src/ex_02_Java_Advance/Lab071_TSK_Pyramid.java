5fc0213cb892f8d997c55136703c6ff7d4aecb9d
vermav27 <verma.vineet27@gmail.com>
2025-02-21 09:39:04 +0530

    pyramid

---
+++ src/ex_02_Java_Advance/Lab071_TSK_Pyramid.java
@@ -0,0 +1,23 @@
+package ex_02_Java_Advance;
+
+public class Lab071_TSK_Pyramid {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 4 ; i++){
+
+            for(int j = 4 ; j > i ; j--){
+                System.out.print(" ");
+            }
+
+            for(int k = 1 ; k <= (2 * i) - 1 ; k++ ){
+                System.out.print("*");
+            }
+
+            System.out.println("\n");
+        }
+
+
+    }
+
+}
