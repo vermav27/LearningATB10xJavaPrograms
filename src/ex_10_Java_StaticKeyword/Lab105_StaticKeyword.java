644dc47bf2e72c5d55d8366c37c7157f3f3c3291
vermav27 <verma.vineet27@gmail.com>
2025-03-10 10:21:35 +0530

    Static Keyword

---
+++ src/ex_10_Java_StaticKeyword/Lab105_StaticKeyword.java
@@ -0,0 +1,19 @@
+package ex_10_Java_StaticKeyword;
+
+public class Lab105_StaticKeyword {
+    public static void main(String[] args) {
+        Student s1 = new Student(23);
+        Student s2 = new Student(43);
+
+        //So this is how we generally call the class variable by creating object.
+        System.out.println(s1.age);
+
+        /*
+        Static keyword means that is "common to all". That means it can be called without
+        creating any object.
+         */
+        System.out.println(Student.course); //Calling variable directly when static keyword is there.
+        Student.study(); //Calling function directly when static keyword is there.
+
+    }
+}
